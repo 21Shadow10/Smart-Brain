9bd7f2dfc2663b3d6422d192b1166a9fdf5b1e2d
Dharmik Govani <63103191+21Shadow10@users.noreply.github.com>
2021-07-15 01:37:47 +0530

    React Components Added- Frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import Particles from 'react-particles-js';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation' ;
+import Logo from './Components/Logo/Logo' ;
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm' ;
+import Rank  from './Components/Rank/Rank';
+
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 300
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <Particles className="particles"
+              params={particleOptions}
+            />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
