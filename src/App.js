5425c8a03911646cd7b6199055a38980548ca630
Dharmik Govani <63103191+21Shadow10@users.noreply.github.com>
2021-07-19 15:35:57 +0530

    FaceRecognition and ImageLink Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React , {Component} from 'react' ;
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai' ;
 import './App.css';
 import Navigation from './Components/Navigation/Navigation' ;
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition' ;
 import Logo from './Components/Logo/Logo' ;
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm' ;
 import Rank  from './Components/Rank/Rank';
 
+const app = new Clarifai.App({
+  apiKey: '0f6f5b5c22e945b2b37c137a37fad8bb',
+})
 
 const particleOptions = {
   particles: {
@@ -18,19 +24,66 @@ const particleOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles"
-              params={particleOptions}
-            />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    //console.log(data) ;
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box ;
+    const image = document.getElementById('inputimage') ;
+    const width = Number(image.width) ;
+    const height = Number(image.height) ;
+    console.log(width,height) ;
+
+    return {
+      leftCol: clarifaiFace.left_col*width ,
+      topRow: clarifaiFace.top_row*height,
+      rightCol: width - (clarifaiFace.right_col*width) ,
+      bottomRow: height - (clarifaiFace.bottom_row*height) 
+    } ;
+  }
+
+  displayFaceBox = (box) => {
+    //console.log(box) ;
+    this.setState({box: box}) ;
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+
+    this.setState({imageUrl: this.state.input})
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className="particles"
+                params={particleOptions}
+              />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.input}/>
+      </div>
+    );
+  }
 }
 
 export default App;
